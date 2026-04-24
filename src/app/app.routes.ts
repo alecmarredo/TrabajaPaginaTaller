e39f99ce38d3b9f914818291f04dd1f78dddd4f4
alecmarredo <alecmarredo@gmail.com>
2026-04-24 11:04:14 -0300

    Clase del 24/4/26

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import path from 'path';
 import { Inicio } from './paginas/inicio/inicio';
 import { InicioSesion } from './auth/inicio-sesion/inicio-sesion';
-import { Nav } from './compartidos/nav/nav';
 import { Productos } from './paginas/productos/productos';
 import { Carrito } from './compartidos/carrito/carrito';
 import { Ofertas } from './paginas/ofertas/ofertas';
@@ -14,10 +12,9 @@ export const routes: Routes = [
     { path: '', component: Inicio },
     { path: 'productos', component: Productos },
     { path: 'carrito', component: Carrito },
-    { path: 'oferta', component: Ofertas },
+    { path: 'ofertas', component: Ofertas },
     { path: 'contacto', component: Contacto },
     { path: 'registro', component: Registro },
     { path: 'favoritos', component: Favoritos },
     { path: 'inicioSesion', component: InicioSesion },
-    { path: 'contacto', component: Contacto },
 ];
