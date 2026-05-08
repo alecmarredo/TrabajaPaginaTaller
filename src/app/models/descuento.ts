0c5ad5c760f2d92b86788b7cfd2bc8086da53aba
alecmarredo <alecmarredo@gmail.com>
2026-05-08 11:05:30 -0300

    clase del 8/5

---
+++ src/app/models/descuento.ts
@@ -0,0 +1,25 @@
+export interface Descuento {
+
+    id: number;
+
+    titulo: string;
+
+    categoria: string;
+
+    descripcion: string;
+
+    descripcion2: string;
+
+    img: string;
+
+    alt: string;
+
+    precioViejo: number;
+
+    precioNuevo: number;
+
+    descuento: string;
+
+    enOferta: boolean;
+
+}
--- src/app/paginas/ofertas/ofertas.ts
+++ src/app/paginas/ofertas/ofertas.ts
@@ -1,9 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Descuento } from '../../models/descuento';
 
 @Component({
   selector: 'app-ofertas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ofertas.html',
   styleUrl: './ofertas.css',
 })
-export class Ofertas {}
+export class Ofertas {
+
+  public infoDescuento: Descuento[];
+  
+    constructor(){
+
+      this.infoDescuento= [
+
+      {
+      id: 1,
+      titulo: 'Buzo Armani Exchange Negro',
+      categoria: 'Buzo',
+      descripcion: 'Buzo premium de algodón, cómodo y elegante.',
+      descripcion2: 'dsds',
+      img: 'assets/IMG_5312.jpeg',
+      alt: 'Buzo Armani Negro',
+      precioViejo: 59.99,
+      precioNuevo: 39.99,
+      descuento: '27%',
+      enOferta: true,
+      },
+
+      ]
+    }
+}
