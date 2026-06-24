7dc0362d3432324b876eb17b908d7632923ab896
alecmarredo <alecmarredo@gmail.com>
2026-06-23 23:44:55 -0300

    trabajo hecho en casa nuevo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,18 @@ import { Contacto } from './paginas/contacto/contacto';
 import { Registro } from './auth/registro/registro';
 import { Favoritos } from './compartidos/favoritos/favoritos';
 import { Compras } from './paginas/compras/compras';
+import { Remeras } from './paginas/productos/secciones/remeras/remeras';
+import { Camperas } from './paginas/productos/secciones/camperas/camperas';
+import { Calzado } from './paginas/productos/secciones/calzado/calzado';
+import { Accesorios } from './paginas/productos/secciones/accesorios/accesorios';
 
 export const routes: Routes = [
     { path: '', component: Inicio },
     { path: 'productos', component: Productos },
+    { path: 'remeras', component: Remeras },
+    { path: 'camperas', component: Camperas },
+    { path: 'calzado', component: Calzado },
+{    path: 'accesorios', component: Accesorios },
     { path: 'carrito', component: Carrito },
     { path: 'ofertas', component: Ofertas },
     { path: 'contacto', component: Contacto },
